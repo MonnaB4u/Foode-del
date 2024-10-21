08505db14a3ae6ebda5940944eb313f16979f9c7
Moheuddin Monna <moheuddin.monnab4u@gmail.com>
2024-10-21 22:50:59 +0800

    finish

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './FoodDisplay.css'
 import { useContext } from 'react'
 import { StoreContext } from '../context/StoreContex'
@@ -6,21 +6,48 @@ import FoodItem from '../FoodItem/FoodItem'
 
 function FoodDisplay({ category }) {
     const { food_list } = useContext(StoreContext)
+
+    const [timeLeft, setTimeLeft] = useState(10);  // Start from 10 seconds
+    const [isTimeUp, setIsTimeUp] = useState(false);  // Flag to check if time is up
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            setIsTimeUp(true);
+            return;
+        }
+        const timer = setInterval(() => {
+            setTimeLeft((prevTime) => prevTime - 1);
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [timeLeft]);
+
+
+
+
+
     return (
 
         <div className="food-display" id="food-display">
             <h2>Top dishes near you</h2>
             <div className="food-display-list">
                 {
-                    food_list.map((item, index) => {
-                        if (category === "All" || category === item.category) {
-                            return (
-                                <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-                            )
-                        }
+                    food_list ?
+                        <p className='apiTimer'>{
+                            timeLeft ===0 ? `please clink on api link first then api will work fine` : `Loading.........${timeLeft}`
+                        }</p>
+                     
+                        :
 
-                    }
-                    )
+                        food_list.map((item, index) => {
+                            if (category === "All" || category === item.category) {
+                                return (
+                                    <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                                )
+                            }
+
+                        }
+                        )
+
                 }
             </div>
         </div>
